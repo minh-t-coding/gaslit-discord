d0b453994fa80da94654386abe43b47f32ca651c
Minh Nguyen <mtnguyen.dev@gmail.com>
2024-08-14 22:28:36 -0700

    installed playroom, added integration and some more styling

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { insertCoin } from 'playroomkit';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+insertCoin({
+  skipLobby: true,
+  gameId: "OoQ0nBe1V6DTnZRKtVl2",
+  discord: true,
+}).then(() =>
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+)
 );
 
 // If you want to start measuring performance in your app, pass a function
